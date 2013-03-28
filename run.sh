c0396200dd03cd405c78b12938ed24b1f9e0a9b9
b-it-bots <b-it-bots@c025-simulation.(none)>
2013-03-28 19:58:24 +0100

    Simple Manipulation Script

---
+++ run.sh
@@ -0,0 +1,4 @@
+openrave.py --database grasping --robot=youbot1.robot.xml --target=tibitsbox1.kinbody.xml --manipulatordirection="0 0 1" --boxdelta=0.01 --standoff=0.002 --standoff=0.015 --graspingnoise=0.01
+
+openrave.py --example graspplanning --scene=youbot4.env.xml  --nodestinations --norandomize
+
