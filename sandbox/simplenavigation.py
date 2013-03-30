2b6ffe28fbf89ad71d9ff6f8f6bac14c6d860cc6
b-it-bots <b-it-bots@c025-simulation.(none)>
2013-03-30 14:27:32 +0100

    Working ... with sleep .. DO NOT DISTURB

--- sandbox/simplenavigation.py
+++ sandbox/simplenavigation.py
@@ -81,68 +81,69 @@ class SimpleNavigationPlanning:
             self.robot.WaitForController(0)
             taskprob = interfaces.TaskManipulation(self.robot)
             taskprob.ReleaseFingers()
-            while True:
-                manip = self.robot.SetActiveManipulator('arm') # set the manipulator to leftarm
-                ikmodel = databases.inversekinematics.InverseKinematicsModel(self.robot,iktype=IkParameterization.Type.TranslationDirection5D)
-                if not ikmodel.load():
-                    ikmodel.autogenerate()
-                with self.env: # lock environment
-                    lower,upper = self.robot.GetDOFLimits(manip.GetArmIndices())
-                    #a = lower+random.rand(len(lower))*(upper-lower)
-                    a = array([-0.88664706, 0.80421693, 0.18288266, 0.20111085, -2.86492377])
-                    self.robot.SetDOFValues(a,manip.GetArmIndices())
-                    ikparam =  IkParameterization('1442840583 -0.5335666042260692 0.8069032587497339 -0.2534044393330962 3.125495570275616 -1.007323359687846 0.4969689507536642 ')
-                    ikparam = manip.GetIkParameterization(IkParameterization.Type.TranslationDirection5D)
-                    sols = manip.FindIKSolutions(ikparam,IkFilterOptions.IgnoreCustomFilters)
-                    Tee = manip.GetEndEffectorTransform()
-                    for i in range(0,sols.shape[0]):
-                        with self.robot:
-                            a = sols[i]
-                            print a
-                            self.robot.SetDOFValues(sols[i],manip.GetArmIndices()) 
-                            self.env.UpdatePublishedBodies()
-                            time.sleep(5)
-                    self.robot.Grab(t)
-		    lower,upper = self.robot.GetDOFLimits(manip.GetArmIndices())
-		    print "back to home position"
-                    #a = lower+random.rand(len(lower))*(upper-lower)
-                    a = array([0,0, 0, 0,0])
-                    self.robot.SetDOFValues(a,manip.GetArmIndices())
-		    self.env.UpdatePublishedBodies()
-                    #taskprob.CloseFingers()
-		
+            
+            manip = self.robot.SetActiveManipulator('arm') # set the manipulator to leftarm
+            ikmodel = databases.inversekinematics.InverseKinematicsModel(self.robot,iktype=IkParameterization.Type.TranslationDirection5D)
+            if not ikmodel.load():
+                ikmodel.autogenerate()
+            with self.env: # lock environment
+                lower,upper = self.robot.GetDOFLimits(manip.GetArmIndices())
+                #a = lower+random.rand(len(lower))*(upper-lower)
+                a = array([-0.88664706, 0.80421693, 0.18288266, 0.20111085, -2.86492377])
+                self.robot.SetDOFValues(a,manip.GetArmIndices())
+                ikparam =  IkParameterization('1442840583 -0.5335666042260692 0.8069032587497339 -0.2534044393330962 3.125495570275616 -1.007323359687846 0.4969689507536642 ')
+                ikparam = manip.GetIkParameterization(IkParameterization.Type.TranslationDirection5D)
+                sols = manip.FindIKSolutions(ikparam,IkFilterOptions.IgnoreCustomFilters)
+                Tee = manip.GetEndEffectorTransform()
+                for i in range(0,sols.shape[0]):
+                    with self.robot:
+                        a = sols[i]
+                        print a
+                        self.robot.SetDOFValues(sols[i],manip.GetArmIndices()) 
+                        self.env.UpdatePublishedBodies()
+                        time.sleep(2)
+                self.robot.Grab(t)
+                lower,upper = self.robot.GetDOFLimits(manip.GetArmIndices())
+                print "back to home position"
+                #a = lower+random.rand(len(lower))*(upper-lower)
+                a = array([0,0, 0, 0,0])
+                self.robot.SetDOFValues(a,manip.GetArmIndices())
+                self.env.UpdatePublishedBodies()
+                #taskprob.CloseFingers()
+    
+                
                     
-                        
-                   
-                while True:
-                        with self.env:
-				            self.robot.SetAffineTranslationLimits(envmin,envmax)
-				            self.robot.SetAffineTranslationMaxVels([0.5,0.5,0.5])
-				            self.robot.SetAffineRotationAxisMaxVels(ones(4))
-				            self.robot.SetActiveDOFs([],DOFAffine.X|DOFAffine.Y|DOFAffine.RotationAxis,[0,0,1])
-				        # pick a random position
-				            with self.robot:
-		                		while True:    
-						            goal = [-1.91587836,0.91536392,-2.00007459]
-						            self.robot.SetActiveDOFValues(goal)
-						            if not self.env.CheckCollision(self.robot):
-						             break
-                        print 'planning to: ',goal
-                            # draw the marker
+               
+           
+            with self.env:
+                self.robot.SetAffineTranslationLimits(envmin,envmax)
+                self.robot.SetAffineTranslationMaxVels([0.5,0.5,0.5])
+                self.robot.SetAffineRotationAxisMaxVels(ones(4))
+                self.robot.SetActiveDOFs([],DOFAffine.X|DOFAffine.Y|DOFAffine.RotationAxis,[0,0,1])
+            # pick a random position
+                with self.robot:
+                    while True:    
+                        goal = [-1.91587836,0.91536392,-2.00007459]
+                        self.robot.SetActiveDOFValues(goal)
+                        if not self.env.CheckCollision(self.robot):
+                         break
+            print 'planning to: ',goal
+                    # draw the marker
 
-                        center = r_[goal[0:2],0.2]
-                        xaxis = 0.5*array((cos(goal[2]),sin(goal[2]),0))
-                        yaxis = 0.25*array((-sin(goal[2]),cos(goal[2]),0))
-                        h = self.env.drawlinelist(transpose(c_[center-xaxis,center+xaxis,center-yaxis,center+yaxis,center+xaxis,center+0.5*xaxis+0.5*yaxis,center+xaxis,center+0.5*xaxis-0.5*yaxis]),linewidth=5.0,colors=array((0,1,0)))
+            center = r_[goal[0:2],0.2]
+            xaxis = 0.5*array((cos(goal[2]),sin(goal[2]),0))
+            yaxis = 0.25*array((-sin(goal[2]),cos(goal[2]),0))
+            h = self.env.drawlinelist(transpose(c_[center-xaxis,center+xaxis,center-yaxis,center+yaxis,center+xaxis,center+0.5*xaxis+0.5*yaxis,center+xaxis,center+0.5*xaxis-0.5*yaxis]),linewidth=5.0,colors=array((0,1,0)))
 
-                        if self.basemanip.MoveActiveJoints(goal=goal,maxiter=3000,steplength=0.1) is None:
-                            print 'retrying...'
-                            continue
-                        print 'waiting for controller'
-                        self.robot.WaitForController(0)
-
-                        break
-            break
+            if self.basemanip.MoveActiveJoints(goal=goal,maxiter=3000,steplength=0.1) is None:
+                print 'retrying...'
+                continue
+            print 'waiting for controller'
+            self.robot.WaitForController(0)
+            self.env.Destroy()
+            time.sleep(10)
+            break   
+ 
         #break         
 
 
@@ -154,7 +155,7 @@ def main(env,options):
     env.Load(options.scene)
     robot = env.GetRobots()[0]
     env.UpdatePublishedBodies()
-    time.sleep(0.1) # give time for environment to update
+    time.sleep(5) # give time for environment to update
     self = SimpleNavigationPlanning(robot)
     self.performNavigationPlanning()
 
@@ -176,4 +177,5 @@ def run(args=None):
     OpenRAVEGlobalArguments.parseAndCreateThreadedUser(options,main,defaultviewer=True)
 
 if __name__ == "__main__":
+    #time.sleep(5)
     run()
